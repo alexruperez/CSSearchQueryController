7db87ddc711662d55fa12ceb7d708a3aff4705e0
alexruperez <alexruperez@gmail.com>
2016-09-22 12:21:10 +0200

    Initial Commit

---
+++ CSSearchQueryController/CSSearchQueryController.h
@@ -0,0 +1,19 @@
+//
+//  CSSearchQueryController.h
+//  CSSearchQueryController
+//
+//  Created by alexruperez on 22/9/16.
+//  Copyright © 2016 alexruperez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CSSearchQueryController.
+FOUNDATION_EXPORT double CSSearchQueryControllerVersionNumber;
+
+//! Project version string for CSSearchQueryController.
+FOUNDATION_EXPORT const unsigned char CSSearchQueryControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSSearchQueryController/PublicHeader.h>
+
+
